1410720e01a415ef1177b5482bb38e0d20acfb75
brunel.lee <brunel.lee@kakaohealthcare.com>
2024-11-07 18:26:58 +0900

    Init

---
+++ Package.swift
@@ -0,0 +1,34 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "DTCore",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "DTCore",
+            targets: ["DTCore"])
+    ],
+    dependencies: [
+        .package(
+            url: "https://github.com/kakaohealthcare-DT/KakaoHealthcareFoundation.git",
+            branch: "main"),
+        .package(url: "https://github.com/kakaohealthcare-DT/DTPersistence.git", branch: "main"),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "DTCore",
+            dependencies: [
+                "KakaoHealthcareUI",
+                "DTPersistence"
+            ]
+        )
+    ]
+)
---
+++ Sources/DTCore/DTCore.swift
@@ -0,0 +1,6 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+@_exported import KakaoHealthcareFoundation
+@_exported import SnapKit
+@_exported import DTPersistence
+@_exported import KakaoHealthcareUI
